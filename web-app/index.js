7dd3a55538b6f29a8effddd3a66a6baf327e9e9a
daniel <daniel@mac.de>
2020-11-18 19:08:42 +0100

    Added changes from rouven

--- web-app/index.js
+++ web-app/index.js
@@ -137,26 +137,26 @@ async function sendTrackingMessage(data) {
 
 function sendResponse(res, html, cachedResult) {
 	res.send(`<!DOCTYPE html>
-	<html lang="en">
+	<html lang<="en">
 	<head>
 	  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 	  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-	  <title>Starter Template - Materialize</title>
+	  <title>Best Webshop in the WWW</title>
 	
 	  <!-- CSS  -->
-	  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-	  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+	  <link rel = "stylesheet" href = "https://fonts.googleapis.com/icon?family=Material+Icons">
+   	  <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">
+   	  
 	</head>
 	<body>
 	  <nav class="light-blue lighten-1" role="navigation">
-		<div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
+		<div class="nav-wrapper container">
 		  <ul class="right hide-on-med-and-down">
-			<li><a href="#">Navbar Link</a></li>
+			<li><a href="#">Login</a></li>
 		  </ul>
 	
 		  <ul id="nav-mobile" class="sidenav">
-			<li><a href="#">Navbar Link</a></li>
+			<li><a href="#">Homepage</a></li>
 		  </ul>
 		  <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 		</div>
@@ -164,23 +164,20 @@ function sendResponse(res, html, cachedResult) {
 	  <div class="section no-pad-bot" id="index-banner">
 		<div class="container">
 		  <br><br>
-		  <h1 class="header center orange-text">Starter Template</h1>
+		  <h1 class="header center blue-text">Webshop for Products</h1>
 		  <div class="row center">
-			<h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-		  </div>
-		  <div class="row center">
-			<a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light orange">Get Started</a>
+			<h5 class="header col s12 light">You have accepted the commercialization of your data. Thank you!</h5>
 		  </div>
 		  <br><br>
-	
 		</div>
 	  </div>
-	
-	
 	  <div class="container">
 		<div class="section">
 	
 		  <!--   Icon Section   -->
+		  <div class="row">
+
+		  </div>
 		  <div class="row">
 			<div class="col s12 m4">
 			  <div class="icon-block">
@@ -252,9 +249,9 @@ function sendResponse(res, html, cachedResult) {
 	
 	
 	  <!--  Scripts-->
+	  <script type = "text/javascript" src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>           
+   	  <script src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js"></script> 
 	  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-	  <script src="js/materialize.js"></script>
-	  <script src="js/init.js"></script>
 	
 	  </body>
 	</html>
